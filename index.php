0dbe74046276f99be05e9e5b07d8232268f72719
Yaron David <urlof_desty91@hotmail.com>
2022-10-07 19:43:47 -0500

    fist commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+    <head> 
+        <title>
+            CICSPractico
+        </title>
+        <link rel="stylesheet" href="./styles/inde.css"/>
+    </head>
+    <body>
+        <div class="titulo">
+            <div class="row">
+                <div class="icono">
+                    <img class="ipn" src="https://onceninasyninos.tv/wp-content/uploads/2018/04/Banner-logos-ipn-e1523046221576.png"/>
+                </div>
+                <div class="text">
+                    <h1>CICSPractico</h1>
+                </div>
+                <div class="icono2">
+                    <img class="cics" src="./images/cics.jpeg"/>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="menu coli">
+                <div class="rowi opc">
+                    <ul class="center">
+                        <a href="./inicioDeSesion.php"><li class="boton">Inicio de sesion</li></a>
+                        <li>Restablecer contraseña</li>
+                    </ul>
+                </div>
+                <div class="rowi info">
+                    <ul class="center">
+                        <li>Info</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="pagina coli">
+                <div class="rowi subtitle">
+                    <h1 class="tab">Pagina principal</h1>
+                </div>
+                <div class="rowi principal">
+                    <div class="card">
+                        <h1>Bienvenido</h1>
+                        <hr/>
+                        <p>Esta pagina tiene es un proyecto que tiene como finalidad agilizar el proceso para la programación y
+                            seguimineto de las practicas y visitas escolares del Centro Interdisciplinario de Ciencias de la Salud
+                            Unidad Santo Tomás del Instituto Politenico Nacional.
+                        </p>
+                        <p>
+                            En caso de contar con una cuenta favor de hacer clic en una de las opciones del lado izquierdo
+                            de lo contrario por favor acude con el coordinador de practicas y visitas para poder crear una cuenta nueva
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ inicioDeSesion.php
@@ -0,0 +1,58 @@
+<html>
+    <head> 
+        <title>
+            CICSPractico
+        </title>
+        <link rel="stylesheet" href="./styles/inde.css"/>
+        <link rel="stylesheet" href="./styles/inicio.css"/>
+    </head>
+    <body>
+        <div class="titulo">
+            <div class="row">
+                <div class="icono">
+                    <img class="ipn" src="https://onceninasyninos.tv/wp-content/uploads/2018/04/Banner-logos-ipn-e1523046221576.png"/>
+                </div>
+                <div class="text">
+                    <h1>CICSPractico</h1>
+                </div>
+                <div class="icono2">
+                    <img class="cics" src="./images/cics.jpeg"/>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="menu coli">
+                <div class="rowi opc">
+                    <ul class="center">
+                        <a href="./index.php"><li class="boton">Pagina principal</li></a>
+                        <li>Restablecer contraseña</li>
+                    </ul>
+                </div>
+                <div class="rowi info">
+                    <ul class="center">
+                        <li>Info</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="pagina coli">
+                <div class="rowi subtitle">
+                    <h1 class="tab">Pagina principal</h1>
+                </div>
+                <div class="rowi principal">
+                    <div class="card">
+                        <h1>Inicio de sesión</h1>
+                        <hr/>
+                        <form>
+                            <label>Usuario</label><br/>
+                            <input type="text" id="usuario" name="usuario" required/><br/>
+                            <label>Contraseña</input><br/>
+                            <input type="text" id="contraseña" name="contraseña" required/><br/>
+                            <input type="submit" value="Iniciar sesión"/>
+                            <a href="./restablecer.php" class="res">No recuerdo mi contraseña</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
