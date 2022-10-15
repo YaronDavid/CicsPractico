700944e661bdbabf88dea214556ad8c5bd010030
Yaron David <urlof_desty91@hotmail.com>
2022-10-15 00:48:07 -0500

    Se cambiaron los estilos por unos basados en bootstrap 5

--- index.php
+++ index.php
@@ -3,55 +3,63 @@
         <title>
             CICSPractico
         </title>
-        <link rel="stylesheet" href="./styles/inde.css"/>
+        <link rel="stylesheet" href="./styles/indee.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
     <body>
-        <div class="titulo">
-            <div class="row">
-                <div class="icono">
+    <div class="container-fluid">
+            <div class="row navegacion">
+                <div class="icono col-1">
                     <img class="ipn" src="https://onceninasyninos.tv/wp-content/uploads/2018/04/Banner-logos-ipn-e1523046221576.png"/>
                 </div>
-                <div class="text">
-                    <h1>CICSPractico</h1>
+                <div class="titulo col-10">
+                    <h1 class="cicsPractico">CICSPractico</h1>
                 </div>
-                <div class="icono2">
+                <div class="icono2 col-1">
                     <img class="cics" src="./images/cics.jpeg"/>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="menu coli">
-                <div class="rowi opc">
+
+        <div class="row main">
+
+            <div class="col-2 aside">
+                <div class="row opciones">
                     <ul class="center">
                         <a href="./inicioDeSesion.php"><li class="boton">Inicio de sesion</li></a>
-                        <li>Restablecer contraseña</li>
+                        <a href="./restablecerContraseña.php"><li>Restablecer contraseña</li></a>
                     </ul>
                 </div>
-                <div class="rowi info">
+                <div class="row info">
                     <ul class="center">
                         <li>Info</li>
                     </ul>
                 </div>
             </div>
-            <div class="pagina coli">
-                <div class="rowi subtitle">
-                    <h1 class="tab">Pagina principal</h1>
+            
+            <div class="col-10">
+                <div class="row subtitulo">
+                    <h1>Pagina principal</h1>
                 </div>
-                <div class="rowi principal">
-                    <div class="card">
-                        <h1>Bienvenido</h1>
-                        <hr/>
-                        <p>Esta pagina tiene es un proyecto que tiene como finalidad agilizar el proceso para la programación y
-                            seguimineto de las practicas y visitas escolares del Centro Interdisciplinario de Ciencias de la Salud
-                            Unidad Santo Tomás del Instituto Politenico Nacional.
-                        </p>
-                        <p>
-                            En caso de contar con una cuenta favor de hacer clic en una de las opciones del lado izquierdo
-                            de lo contrario por favor acude con el coordinador de practicas y visitas para poder crear una cuenta nueva
-                        </p>
+                <div class="row principal">
+                    <div class="container">
+                        <div class="mycard">
+                            <h1>Bienvenido</h1>
+                            <hr/>
+                            <p>Esta pagina tiene es un proyecto que tiene como finalidad agilizar el proceso para la programación y
+                                seguimineto de las practicas y visitas escolares del Centro Interdisciplinario de Ciencias de la Salud
+                                Unidad Santo Tomás del Instituto Politenico Nacional.
+                            </p>
+                            <p>
+                                En caso de contar con una cuenta favor de hacer clic en una de las opciones del lado izquierdo
+                                de lo contrario por favor acude con el coordinador de practicas y visitas para poder crear una cuenta nueva
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div class="w-100"></div>
+
         </div>
+</div>
     </body>
 </html>
