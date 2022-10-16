7966c7a5774f75889ba284b1ecdd46f1ab484cf1
Yaron David Esquivel Ventura <Urlof_desty91@hotmail.com>
2022-10-16 01:34:12 -0500

    "Se agrego bootstrap a inicioDeSesion.php"
    Se borro el comentario de indee.css donde los estilos no se basaban en bootstrap y se paso el contenido de inicio.css a ini.css por un error local

--- inicioDeSesion.php
+++ inicioDeSesion.php
@@ -1,66 +1,70 @@
-<?php
-    include_once("conexion.php");
-
-    $conn=Cconection::conectar();
-
-?>
-
 <html>
     <head> 
         <title>
             CICSPractico
         </title>
-        <link rel="stylesheet" href="./styles/inde.css"/>
-        <link rel="stylesheet" href="./styles/inicio.css"/>
-        
+        <link rel="stylesheet" href="./styles/indee.css"/>
+        <link rel="stylesheet" href="./styles/ini.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
     <body>
-        <div class="titulo">
-            <div class="row">
-                <div class="icono">
-                    <img class="ipn" src="https://onceninasyninos.tv/wp-content/uploads/2018/04/Banner-logos-ipn-e1523046221576.png"/>
+        <div class="container-fluid">
+            <div class="row navegacion">
+                    <div class="icono col-1">
+                        <img class="ipn" src="https://onceninasyninos.tv/wp-content/uploads/2018/04/Banner-logos-ipn-e1523046221576.png"/>
+                    </div>
+                    <div class="titulo col-10">
+                        <h1 class="cicsPractico">CICSPractico</h1>
+                    </div>
+                    <div class="icono2 col-1">
+                        <img class="cics" src="./images/cics.jpeg"/>
+                    </div>
                 </div>
-                <div class="text">
-                    <h1>CICSPractico</h1>
-                </div>
-                <div class="icono2">
-                    <img class="cics" src="./images/cics.jpeg"/>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="menu coli">
-                <div class="rowi opc">
+
+        <div class="row main">
+
+            <div class="col-2 aside">
+                <div class="row opciones">
                     <ul class="center">
                         <a href="./index.php"><li class="boton">Pagina principal</li></a>
                         <a href="./restablecerContraseña.php"><li>Restablecer contraseña</li></a>
                     </ul>
                 </div>
-                <div class="rowi info">
+                <div class="row info">
                     <ul class="center">
                         <li>Info</li>
                     </ul>
                 </div>
             </div>
-            <div class="pagina coli">
-                <div class="rowi subtitle">
-                    <h1 class="tab">Pagina principal</h1>
+
+            <div class="col-10">
+                <div class="row subtitulo">
+                    <h1>Pagina principal</h1>
                 </div>
-                <div class="rowi principal">
-                    <div class="card">
-                        <h1>Inicio de sesión</h1>
-                        <hr/>
-                        <form>
-                            <label>Usuario</label><br/>
-                            <input type="text" id="usuario" name="usuario" required/><br/>
-                            <label>Contraseña</input><br/>
-                            <input type="text" id="contraseña" name="contraseña" required/><br/>
-                            <input type="submit" value="Iniciar sesión"/>
-                            <a href="./restablecer.php" class="res">No recuerdo mi contraseña</a>
-                        </form>
+                <div class="row principal">
+                    <div class="container">
+                        <div class="mycard">
+                            <h1>Inicio de sesión</h1>
+                            <hr/>
+                            <form action="validar.php" method="post">
+                                <div class="form-group">
+                                    <label for="usuario">Usuario</label>
+                                    <input class="form-control"type="text" id="usuario" name="usuario" required/>
+                                </div>
+                                <div class="form-group">
+                                    <label for="contrasena">Contraseña</label>
+                                    <input class="form-control" type="text" id="contrasena" name="contrasena" required/>
+                                </div><br/>
+                                    <input type="submit" value="Iniciar sesión" class="btn btn-primary"/>
+                                    <a href="./restablecer.php" class="res">No recuerdo mi contraseña</a>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div class="w-100"></div>
+
         </div>
+</div>
     </body>
 </html>
